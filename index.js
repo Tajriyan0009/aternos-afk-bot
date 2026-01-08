2383560c0c4c871398831077d022023c7360440d
Tajriyan0009 <kingofmoney5321@gmail.com>
2026-01-08 14:35:47 +0500

    Update bot server IP and port configuration

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
-  host: 'YOUR_SERVER_IP', // without port (ex: play.aternos.me)
-  port: 25565, // Default Minecraft port
+  host: 'AashirS.aternos.me', // without port (ex: play.aternos.me)
+  port: 36944, // Default Minecraft port
   username: 'AFK_Bot' // Bot username
 });
 
